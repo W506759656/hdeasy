87fc335762aefbac1c799bc8ef5805811554b8ac
Wangding <506759656@qq.com>
2018-06-06 18:33:51 +0800

    .

--- hdeasy/resources/views/home.blade.php
+++ hdeasy/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="panel panel-default" style="text-align: center;width: 80%;">
+        <div class="panel panel-default" style="text-align: center;width: 100%;">
             <img style="width: 100%;" src="{{asset('images/rg.jpg')}}" alt="">
         </div>
     </div>
