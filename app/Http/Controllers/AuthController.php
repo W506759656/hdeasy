6493709fb37089d4b13e48bc98e2250528e2d670
Wangding <506759656@qq.com>
2018-06-08 15:37:52 +0800

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,27 +53,8 @@ class AuthController extends Controller
 
     public function getAuthenticatedUser()
     {
-        try {
-            $jwtauth = new JWTAuth();
-            if (! $user = $jwtauth->parseToken()->authenticate()) {
-                return response()->json(['user_not_found'], 404);
-            }
-
-        } catch (TokenExpiredException $e) {
-
-            return response()->json(['token_expired'], $e->getStatusCode());
-
-        } catch (TokenInvalidException $e) {
-
-            return response()->json(['token_invalid'], $e->getStatusCode());
-
-        } catch (JWTException $e) {
-
-            return response()->json(['token_absent'], $e->getStatusCode());
-
-        }
-
+        $user = Auth::user();
+        return $user;
         // the token is valid and we have found the user via the sub claim
-        return response()->json(compact('user'));
     }
 }
